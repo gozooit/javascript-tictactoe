e812247ce415c0c04bae02a338736db7f454e1ec
gozooit <gozoo.it@protonmail.com>
2023-01-02 18:20:44 +0100

    updated

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ const gameBoard = (() => {
   let firstRow = ["","",""];
   let secondRow = ["","",""];
   let thirdRow = ["","",""];
-  const board = [firstRow, secondRow, thirdRow]
+  const board = [firstRow, secondRow, thirdRow];
   const addSymbol = (symbol, positionNumber, rowNumber) => {
     position = convertPosition(positionNumber);
     row = convertRow(rowNumber);
@@ -28,6 +28,34 @@ const gameBoard = (() => {
   };
 })();
 
+// const displayController = (() => {
+//   const container = document.createElement('div');
+//   container.setAttribute('id', 'board-container');
+//   container.setAttribute('class', 'container');
+
+//   container.board.forEach({
+
+//   });
+// })();
+
+const theGame = (() => {
+
+})();
+
+const Player = (name, symbol) => {
+  return { name, symbol };
+};
+
+const p1 = Player('gozoo', 'X');
+const p2 = Player('gwen', 'O');
+
 console.log(gameBoard.board);
-gameBoard.addSymbol("x", 1, 1);
+gameBoard.addSymbol(p1.symbol, 1, 1);
 console.log(gameBoard.board);
+gameBoard.addSymbol(p2.symbol, 2, 2);
+console.log(gameBoard.board);
+gameBoard.addSymbol(p1.symbol, 3, 3);
+console.log(gameBoard.board);
+gameBoard.addSymbol(p2.symbol, 1, 3);
+console.log(gameBoard.board);
+console.log(gameBoard.firstRow);
