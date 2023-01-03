18053e79804b0960058c439277b2b56bab63d071
gozooit <gozoo.it@protonmail.com>
2023-01-03 15:03:18 +0100

    add displayController logic

--- application.js
+++ application.js
@@ -28,23 +28,54 @@ const gameBoard = (() => {
   };
 })();
 
-// const displayController = (() => {
-//   const container = document.createElement('div');
-//   container.setAttribute('id', 'board-container');
-//   container.setAttribute('class', 'container');
-
-//   container.board.forEach({
-
-//   });
-// })();
-
-const theGame = (() => {
-
+const displayController = (() => {
+  const create = () => {
+    const container = document.createElement('div');
+    container.setAttribute('id', 'board-container');
+    container.setAttribute('class', 'container');
+  
+    gameBoard.board.forEach((row, rowIndex) => {
+      const rowElement = document.createElement('div');
+      rowElement.setAttribute('class', 'row');
+      row.forEach((cell, cellIndex) => {
+        const cellElement = document.createElement('div');
+        cellElement.setAttribute('class', 'cell');
+        cellElement.innerText = cell;
+        cellElement.addEventListener('click', () => {
+          // Add the current player's symbol to the cell
+          gameBoard.addSymbol(currentPlayer.symbol, cellIndex + 1, rowIndex + 1);
+          // Update the cell's text to display the symbol
+          cellElement.innerText = currentPlayer.symbol;
+        });
+        rowElement.appendChild(cellElement);
+      });
+      container.appendChild(rowElement);
+    });
+    document.body.appendChild(container);
+  };
+  const update = () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell, index) => {
+      const row = Math.floor(index / 3);
+      const col = index % 3;
+      cell.innerText = gameBoard.board[row][col];
+    });
+  };
+  return {
+    create,
+    update,
+  }
 })();
 
 const Player = (name, symbol) => {
   return { name, symbol };
 };
+
+const theGame = (() => {
+
+})();
+
+displayController.create();
 
 const p1 = Player('gozoo', 'X');
 const p2 = Player('gwen', 'O');
@@ -59,3 +90,5 @@ console.log(gameBoard.board);
 gameBoard.addSymbol(p2.symbol, 1, 3);
 console.log(gameBoard.board);
 console.log(gameBoard.firstRow);
+
+displayController.update();
